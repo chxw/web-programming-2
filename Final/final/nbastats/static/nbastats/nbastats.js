56f6be417aefd82ffdd40321b77a5b9e28308912
Chelsea Wang <chexuhuwa@gmail.com>
2021-08-04 21:03:14 -0400

    ADD: non-working line chart with dropdown

--- Final/final/nbastats/static/nbastats/nbastats.js
+++ Final/final/nbastats/static/nbastats/nbastats.js
@@ -37,15 +37,16 @@ function displayGraph() {
         let bio = document.querySelector('.bio');
         bio.style.display = 'none';
 
-        let graph = document.querySelector('.graph');
-        graph.style.display = 'block';
+        let graphDiv = document.querySelector('.graph-div');
+        graphDiv.style.display = 'block';
 
         // set the dimensions and margins of the graph
-        let margin = {top: 10, right: 30, bottom: 30, left: 60},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        let margin = {top: 10, right: 100, bottom: 30, left: 30},
+            width = 460 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
 
-        let svg = d3.select('.graph')
+        // append the svg object to the body of the page
+        let svg = d3.select(".graph")
             .append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
@@ -53,30 +54,83 @@ function displayGraph() {
                 .attr("transform",
                     "translate(" + margin.left + "," + margin.top + ")");
 
+        // Group selections
+        let attributes = Object.keys(data[0]);
+
+        console.log(data[0]);
+
+        // add the options to the button
+        d3.select("#selectButton")
+            .selectAll('myOptions')
+                .data(attributes)
+            .enter()
+                .append('option')
+            .text(function (d) { return d; }) // text showed in the menu
+            .attr("value", function (d) { return d; }); // corresponding value returned by the button
+
+        // A color scale: one color for each group
+        let myColor = d3.scaleOrdinal()
+            .domain(attributes)
+            .range(d3.schemeSet2);
+
         // Add X axis --> it is a date format
-        const x = d3.scaleTime()
-            .domain(d3.extent(data, function(d) { return d.year; }))
+        let x = d3.scaleLinear()
+            .domain([0,10])
             .range([ 0, width ]);
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
-        
+
         // Add Y axis
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(data, function(d) { return +d.apg; })])
+        let y = d3.scaleLinear()
+            .domain( [0,20])
             .range([ height, 0 ]);
         svg.append("g")
             .call(d3.axisLeft(y));
-
-        // Add the line
-        svg.append("path")
+        
+        // Initialize line with attributes[0]
+        let line = svg
+        .append('g')
+        .append("path")
             .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
             .attr("d", d3.line()
-                .x(function(d) { return x(d.year); })
-                .y(function(d) { return y(d.apg); })
-            );
+            .x(function(d) { return x(+d.year); })
+            .y(function(d) { return y(+d.min); })
+            )
+            .attr("stroke", function(d){ return myColor(d.min); })
+            .style("stroke-width", 4)
+            .style("fill", "none");
+        
+        // A function that update the chart
+        function update(selectedGroup) {
+
+            // Create new data with the selection?
+            var dataFilter = data.map(function(d){return {year: d.year, value:d[selectedGroup]}; });
+    
+            // Give these new data to update line
+            line
+                .datum(dataFilter)
+                .transition()
+                .duration(1000)
+                .attr("d", d3.line()
+                .x(function(d) { return x(+d.year); })
+                .y(function(d) { return y(+d.value); })
+                )
+                .attr("stroke", function(d){ return myColor(selectedGroup); });
+        }
+    
+        // When the button is changed, run the updateChart function
+        d3.select("#selectButton").on("change", function(d) {
+            // recover the option that has been chosen
+            var selectedOption = d3.select(this).property("value");
+            // run the updateChart function with this selected option
+            update(selectedOption);
+        });
     }
+}
+
+function getPropertyName(obj, expression) {
+    var res = {};
+    Object.keys(obj).map(k => { res[k] = () => k; });
+    return expression(res)();
 }
